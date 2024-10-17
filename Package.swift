0c8c3f23c00ad8e1873f82a80a082c3231a72da6
Dominik Ehret <ahoi@captaincode.ch>
2024-10-17 15:31:57 +0200

    New release with raster particle support

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .binaryTarget(
             name: "MapLibre",
             url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/main/MapLibre.dynamic.xcframework.zip",
-            checksum: "a92b20c512f77b233ee2c60c3d4dac389ac11a1bbaed51e3977116f9785952bd")
+            checksum: "ed459fe8c8b80eb45628fc3b7f4f3d55a966b7a0a2678e094692f035981effbf")
     ]
 )
