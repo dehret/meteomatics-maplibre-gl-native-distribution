8f65b8e7193b08638926b75c1ddbf787ffc55d6f
Dominik Ehret <ahoi@captaincode.ch>
2024-10-23 13:46:29 +0200

    Added permalink

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/1f6d6029c53d0a57506318f8493ea29938acf4b1/MapLibre.dynamic.xcframework.zip",
+            url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/blob/a24df4a8075b620fdc04f582f10ce5ce03952447/MapLibre.dynamic.xcframework.zip",
             checksum: "2201916a1b6b5e72c486f236670bc860160d66ac5233810636b66b160efe9110")
     ]
 )
