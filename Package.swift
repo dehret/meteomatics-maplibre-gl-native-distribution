8485c4c4bb717dfda57515005cb50611710b6f28
Dominik Ehret <ahoi@captaincode.ch>
2024-10-25 09:02:29 +0200

    Updated download url

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/a24df4a8075b620fdc04f582f10ce5ce03952447/MapLibre.dynamic.xcframework.zip",
+            url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/7f6fc6899e41e0f3565b28ff3ebd36cc404d42d4/MapLibre.dynamic.xcframework.zip",
             checksum: "f5e405441fcf96366b5f5ee7e4396df5bc6abf67f19a67bebcc4ada3ed0febe4")
     ]
 )
