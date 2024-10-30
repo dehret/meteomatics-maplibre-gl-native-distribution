abf6b0434a9a254e36e8044c337122f234508c0b
Dominik Ehret <ahoi@captaincode.ch>
2024-10-30 15:20:29 +0100

    New build

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .binaryTarget(
             name: "MapLibre",
             url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/f49a55a710108edce6d78aa9010c22b88bfb8113/MapLibre.dynamic.xcframework.zip",
-            checksum: "005a49bbfd9170c3cb2602728f76ae4e27fb66fdde2981671d695d147a69cd93")
+            checksum: "c372cf87061c00aa1ee5d2d62ec0a6f3fee5095b4059605d7d95606965e99859")
     ]
 )
