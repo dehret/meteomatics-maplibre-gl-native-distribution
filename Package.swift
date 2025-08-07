19ff1d7ef66179ce2072951141e4c377e3ff2601
Dominik Ehret <ahoi@captaincode.ch>
2025-08-07 15:37:39 +0200

    Uploaded new binary for 6.7.0-cc-12

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .binaryTarget(
             name: "MapLibre",
             url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/be231a53d0de84368c96a616fba85d8e3b73cee0/MapLibre.dynamic.xcframework.zip",
-            checksum: "d88d4e4197a5b217e62e2c22bf361b489cdf3b6ae7d589be4e6e9d1719f188a0")
+            checksum: "5d0e104af2b930562437207478d6f3b3c3635d10c584730820ba4888e79e22a4")
     ]
 )
