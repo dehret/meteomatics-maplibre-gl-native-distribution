92b897050c2a92851d7809cd65abfb90c2e455a3
Dominik Ehret <ahoi@captaincode.ch>
2025-08-07 14:06:00 +0200

    Fixed checksum

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/cb1e8bffc132ccc186b95e69da574beb55499aaa/MapLibre.dynamic.xcframework.zip",
+            url: "https://github.com/dehret/meteomatics-maplibre-gl-native-distribution/raw/a2a868798e164825c8afb195f36111b1a805bf13/MapLibre.dynamic.xcframework.zip",
             checksum: "bce090641bfa9f5f30d6cf2a9e538e539f62a4c4f6da2339fa38d287e4b31d01")
     ]
 )
